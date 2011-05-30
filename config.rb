61ffe3037f7ba2a87631234318879774c9064863
Javier Arce <javierarce@vizzuality.com>
2011-05-30 17:25:32 +0200

    Compass added

--- config.rb
+++ config.rb
@@ -1,11 +1,13 @@
+require 'html5-boilerplate'
 # Require any additional compass plugins here.
+
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -19,6 +21,7 @@ javascripts_dir = "javascripts"
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
+preferred_syntax = :sass
+
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass

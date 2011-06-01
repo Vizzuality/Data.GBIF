30a86c443cad3d7efe10f2afad75a83e074e006a
Javier Arce <javierarce@vizzuality.com>
2011-06-01 19:31:14 +0200

    Starting to implement the map

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$(function(){
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var myOptions = {
+    zoom: 8,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map"), myOptions);
+});

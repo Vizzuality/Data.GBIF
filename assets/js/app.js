a1593a7982d991c0f0790985f09a11930d49cbff
Javier Arce <javierarce@vizzuality.com>
2011-06-10 18:52:49 +0200

    Added uniform.js to style the select boxes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 $(function(){
+  $("select").uniform();
   var latlng = new google.maps.LatLng(-34.397, 150.644);
   var myOptions = {
     zoom: 8,
@@ -7,6 +8,7 @@ $(function(){
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map"), myOptions);
+
 
 //  $('.search_button, .candy_white_button, .candy_blue_button').mousedown(function() { $(this).addClass('active'); });
 //  $('.search_button, .candy_white_button, .candy_blue_button').mouseup(function() { $(this).removeClass('active'); });

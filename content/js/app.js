d472c8f300857fa8c0200236e954e817ee9b7b98
Javier Arce <javierarce@vizzuality.com>
2011-06-28 11:47:56 +0200

    Added map to ocurrrences detail

--- content/js/app.js
+++ content/js/app.js
@@ -250,16 +250,16 @@ $(function(){
     if (e.keyCode == 27) { infoWindow.hide(); }   // esc
   });
 
-  if ($("#map").length ) {
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-    var myOptions = {
-      zoom: 8,
-      center: latlng,
-      disableDefaultUI: true,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map"), myOptions);
-  }
+//  if ($("#map").length ) {
+//    var latlng = new google.maps.LatLng(-34.397, 150.644);
+//    var myOptions = {
+//      zoom: 8,
+//      center: latlng,
+//      disableDefaultUI: true,
+//      mapTypeId: google.maps.MapTypeId.ROADMAP
+//    };
+//    var map = new google.maps.Map(document.getElementById("map"), myOptions);
+//  }
 
   //  $('.search_button, .candy_white_button, .candy_blue_button').mousedown(function() { $(this).addClass('active'); });
   //  $('.search_button, .candy_white_button, .candy_blue_button').mouseup(function() { $(this).removeClass('active'); });

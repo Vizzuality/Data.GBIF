e9842542785122bc49efc13f94e7a2095ca80c72
Javier Arce <javierarce@vizzuality.com>
2011-07-12 17:48:11 +0200

    Added minigraphs

--- content/js/app.js
+++ content/js/app.js
@@ -10,8 +10,8 @@ $(function(){
     var values = [];
 
     for (var i=0; i<=limit; i++) {
-      if (Math.random()*100 > 90) {
-        values[i] = Math.floor(Math.random()*20);
+      if (Math.random()*100 > 80) {
+        values[i] = Math.floor(Math.random()*20 + 5);
       } else {
         values[i] = last;
       }
@@ -268,6 +268,93 @@ $(function(){
   };
 
   $("article#slideshow-1").bindSlideshow();
+
+  function generateRandomValues2(limit) {
+    var last = 0;
+    var values = [];
+
+    for (var i=0; i<=limit; i++) {
+        values[i] = Math.floor(Math.random()*20);
+    }
+    return values;
+  }
+
+  var values = generateRandomValues2(365);
+
+
+  function drawMiniGraph(holder, data) {
+    function getAnchors(p1x, p1y, p2x, p2y, p3x, p3y) {
+        var l1 = (p2x - p1x) / 2,
+            l2 = (p3x - p2x) / 2,
+            a = Math.atan((p2x - p1x) / Math.abs(p2y - p1y)),
+            b = Math.atan((p3x - p2x) / Math.abs(p2y - p3y));
+        a = p1y < p2y ? Math.PI - a : a;
+        b = p3y < p2y ? Math.PI - b : b;
+        var alpha = Math.PI / 2 - ((a + b) % (Math.PI * 2)) / 2,
+            dx1 = l1 * Math.sin(alpha + a),
+            dy1 = l1 * Math.cos(alpha + a),
+            dx2 = l2 * Math.sin(alpha + b),
+            dy2 = l2 * Math.cos(alpha + b);
+        return {
+            x1: p2x - dx1,
+            y1: p2y + dy1,
+            x2: p2x + dx2,
+            y2: p2y + dy2
+        };
+    }
+    // Draw
+    var width = 289,
+        height = 113,
+        leftgutter = 0,
+        bottomgutter = 15,
+        topgutter = 0,
+        color = "#E5E5E5",
+        r = Raphael(holder, width, height),
+        X = (width - leftgutter) / data.length,
+        max = Math.max.apply(Math, data),
+        Y = (height - bottomgutter - topgutter) / max;
+
+    var path = r.path().attr({ stroke: "none"}),
+        bgp = r.path().attr({ stroke: "none", fill: color }),
+        blanket = r.set();
+
+    var p, bgpp;
+
+    for (var i = 0, ii = data.length; i < ii; i++) {
+        var y = Math.round(height - bottomgutter - Y * data[i]),
+            x = Math.round(leftgutter + X * (i + .5));
+        if (!i) {
+            p = ["M", x, y, "C", x, y];
+            bgpp = ["M", leftgutter + X * .5, height - bottomgutter, "L", x, y, "C", x, y];
+        }
+        if (i && i < ii - 1) {
+            var Y0 = Math.round(height - bottomgutter - Y * data[i - 1]),
+                X0 = Math.round(leftgutter + X * (i - .5)),
+                Y2 = Math.round(height - bottomgutter - Y * data[i + 1]),
+                X2 = Math.round(leftgutter + X * (i + 1.5));
+            var a = getAnchors(X0, Y0, x, y, X2, Y2);
+            p = p.concat([a.x1, a.y1, x, y, a.x2, a.y2]);
+            bgpp = bgpp.concat([a.x1, a.y1, x, y, a.x2, a.y2]);
+        }
+        blanket.push(r.rect(leftgutter + X * i, 0, X, height - bottomgutter).attr({
+            stroke: "none",
+            fill: "#fff",
+            opacity: 0
+        }));
+        var rect = blanket[blanket.length - 1];
+    }
+
+    p = p.concat([x, y, x, y]);
+    bgpp = bgpp.concat([x, y, x, y, "L", x, height - bottomgutter, "z"]);
+    path.attr({ path: p });
+    bgp.attr({ path: bgpp });
+  }
+
+  drawMiniGraph("dataset-graph1", generateRandomValues(50));
+  drawMiniGraph("dataset-graph2", generateRandomValues(50));
+  drawMiniGraph("dataset-graph3", generateRandomValues(50));
+  drawMiniGraph("dataset-graph4", generateRandomValues(50));
+
 
   //  $(".select-filter ul").jScrollPane({ verticalDragMinHeight: 20});
   //  $('.search_button, .candy_white_button, .candy_blue_button').mousedown(function() { $(this).addClass('active'); });

fa5fe5eab03e4123aa594e5097ba225ae4da4e4a
Javier Arce <javierarce@vizzuality.com>
2011-07-15 17:31:38 +0200

    Added stats/activities

--- content/js/app.js
+++ content/js/app.js
@@ -1,9 +1,8 @@
 var is_ie = $.browser.msie;
 
 String.prototype.toProperCase = function () {
-    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
-
 
 $(function(){
   var values = generateRandomValues(365);
@@ -88,49 +87,6 @@ $(function(){
     };
   })();
 
-  //$.fn.bindFilterPopover = function(opt) {
-  //  $(this).live("click", function(event) {
-  //    var c = filterPopover();
-  //    c.toggle($(this), event, opt);
-  //  });
-  //};
-
-  $.fn.bindDownloadPopover = function(opt) {
-    $(this).click(function(event) {
-      downloadPopover.toggle($(this), event, opt);
-    });
-  };
-
-  $.fn.bindLoginPopover = function(opt) {
-    $(this).click(function(event) {
-      loginPopover.toggle($(this), event, opt);
-    });
-  };
-
-  $.fn.bindSortPopover = function() {
-    $(this).click(function(event) {
-      sortPopover.toggle($(this), event);
-    });
-  };
-
-  $.fn.bindLinkPopover = function(opt) {
-    $(this).click(function(event) {
-      linkPopover.toggle($(this), event, opt);
-    });
-  };
-
-  $.fn.bindHelpPopover = function(opt) {
-    $(this).click(function(event) {
-      helpPopover.toggle($(this), event, opt);
-    });
-  };
-
-  $.fn.bindDatePopover = function() {
-    $(this).click(function(event) {
-      datePopover.toggle($(this), event);
-    });
-  };
-
   // Bindings
   var c = filterPopover();
   $("a.filter").click(function(event){
@@ -145,36 +101,11 @@ $(function(){
 
   $("time.selectable").bindDatePopover();
   $("a.help").bindHelpPopover({title:"Hi, I'm a yellow popover", message:"This is a <strong>message</strong> with a <a href='http://www.gbif.org/'>link</a>."});
-  $("a.download").bindDownloadPopover({explanation:"Occurrences of \"Puma concolor\", collected between Jan 1sr, 2000 and Jan 1st, 2010, from dataset \"Felines of the world\"."});
+    $("a.download").bindDownloadPopover({explanation:"Occurrences of \"Puma concolor\", collected between Jan 1sr, 2000 and Jan 1st, 2010, from dataset \"Felines of the world\"."});
   $("a.download_2").bindDownloadPopover({template: "direct_download", explanation:"Occurrences of \"Puma concolor\", collected between Jan 1sr, 2000 and Jan 1st, 2010, from dataset \"Felines of the world\"."});
   $("a.login").bindLoginPopover();
 
   $('.sort').bindSortPopover();
-
-
-//$(".autocomplete input").autocomplete({
-//  close: function(event, ui) {
-//  },
-//  create: function(event, ui) {
-//    $(".ui-autocomplete").append("<div class='inner'></div>");
-//  },
-//  open: function(event, ui) {
-//
-//    $("ul.ui-autocomplete").find("li:first-child").addClass("first");
-//
-//        //$('.ui-autocomplete .inner').addClass('scroll-pane').jScrollPane();
-//       // $('.jScrollPaneContainer').css({
-//       //   'position': 'absolute',
-//       //   'top': ($(this).offset().top + $(this).height() + 5) + 'px',
-//       //   'left': $(this).offset().left + 'px'
-//       // });
-//
-//
-//  },
-//  source: [ { label: 'Puma Concolor', desc: 'Species' }, { label: 'Puma Concolor Mexicanensis', desc: 'Subspecies'}, { label: 'Puma Concolor', desc: 'Family '} ]
-//}).data("autocomplete")._renderItem = function(ul, item) {
-//  return $("<li></li>").data("item.autocomplete", item).append("<a>" + item.label + "</a>" + item.desc).appendTo(ul);
-//};
 
   $(document).keyup(function(e) {
     if (e.keyCode == 27) { // esc key
@@ -194,73 +125,18 @@ $(function(){
     selectBox.toggle($(this).parent(), e);
   });
 
-  $( "#slider-range" ).slider({
-    range: true,
-    min: 0,
-    max: 500,
-    values: [ 75, 300 ],
-    slide: function( event, ui ) {
-      $( "#range" ).val( "BETWEEN " + ui.values[ 0 ] + " AND " + ui.values[ 1 ] );
-    }
-  });
-
-  $( "#range" ).val( "BETWEEN " + $( "#slider-range" ).slider( "values", 0 ) + " AND " + $( "#slider-range" ).slider( "values", 1 ) );
-
-  $.fn.bindSlideshow = function(opt) {
-    var $this = $(this);
-    var photo_width = 627;
-    var currentPhoto = 0;
-    var num_of_photos = $this.find("div.photos > img").length - 1;
-    var downloads = $this.find("div.download a");
-
-    var $previous_button = $this.find(".previous_slide");
-    var $next_button = $this.find(".next_slide");
-
-    $previous_button.addClass("disabled");
-
-    $this.find(".photos").width(num_of_photos*photo_width);
-
-    $previous_button.click(function(event) {
-      event.preventDefault();
-      if (currentPhoto > 0) {
-        $next_button.removeClass("disabled");
-        $this.find('.slideshow').scrollTo('-=627px', 500,{easing:'easeOutQuart', axis:'x'});
-        $(downloads[currentPhoto]).parent().hide();
-        currentPhoto--;
-        $(downloads[currentPhoto]).parent().show();
-
-        if (currentPhoto == 0) {
-          $previous_button.addClass("disabled");
-        }
-      }
-    });
-
-    $next_button.click(function(event) {
-      event.preventDefault();
-      if (currentPhoto < num_of_photos) {
-        $previous_button.removeClass("disabled");
-
-        $this.find('div.content div.slideshow').scrollTo("+=627px", 500, {easing:'easeOutQuart', axis:'x'});
-        $(downloads[currentPhoto]).parent().hide();
-        currentPhoto++;
-        $(downloads[currentPhoto]).parent().show();
-
-        if (currentPhoto >= num_of_photos) {
-          $next_button.addClass("disabled");
-        }
-
-      }
-    });
-  };
+  $(".range").bindSlider(0, 500, [0, 500]);
 
   $("article#slideshow-1").bindSlideshow();
 
   function generateRandomValues2(limit) {
     var last = 0;
     var values = [];
+    var random = 100;
 
     for (var i=0; i<=limit; i++) {
-        values[i] = Math.floor(Math.random()*20);
+      random = Math.floor(Math.random()*20);
+      values[i] = random;
     }
     return values;
   }
@@ -272,72 +148,74 @@ $(function(){
     var container_id = ob.attr("id");
 
     function getAnchors(p1x, p1y, p2x, p2y, p3x, p3y) {
-        var l1 = (p2x - p1x) / 2, l2 = (p3x - p2x) / 2;
-        var a = Math.atan((p2x - p1x) / Math.abs(p2y - p1y));
-        var b = Math.atan((p3x - p2x) / Math.abs(p2y - p3y));
+      var l1 = (p2x - p1x) / 2, l2 = (p3x - p2x) / 2;
+      var a = Math.atan((p2x - p1x) / Math.abs(p2y - p1y));
+      var b = Math.atan((p3x - p2x) / Math.abs(p2y - p3y));
 
-        a = p1y < p2y ? Math.PI - a : a;
-        b = p3y < p2y ? Math.PI - b : b;
+      a = p1y < p2y ? Math.PI - a : a;
+      b = p3y < p2y ? Math.PI - b : b;
 
-        var alpha = Math.PI / 2 - ((a + b) % (Math.PI * 2)) / 2;
-        var dx1 = l1 * Math.sin(alpha + a);
-        var dy1 = l1 * Math.cos(alpha + a);
-        var dx2 = l2 * Math.sin(alpha + b);
-        var dy2 = l2 * Math.cos(alpha + b);
+      var alpha = Math.PI / 2 - ((a + b) % (Math.PI * 2)) / 2;
+      var dx1 = l1 * Math.sin(alpha + a);
+      var dy1 = l1 * Math.cos(alpha + a);
+      var dx2 = l2 * Math.sin(alpha + b);
+      var dy2 = l2 * Math.cos(alpha + b);
 
-        return {
-            x1: p2x - dx1,
-            y1: p2y + dy1,
-            x2: p2x + dx2,
-            y2: p2y + dy2
-        };
+      return {
+        x1: p2x - dx1,
+        y1: p2y + dy1,
+        x2: p2x + dx2,
+        y2: p2y + dy2
+      };
     }
 
     if (!opt) {
       var opt = {};
     }
 
-    // Dimensions
-    var width = $("#"+container_id).width();
+    // Configuration
+    var width  = opt.width  || $("#"+container_id).width();
     var height = opt.height || 163;
 
-    // Gutter
-    var leftGutter = opt.leftGutter || 0;
-    var bottomGutter = opt.bottomGutter || 15;
-    var topGutter = opt.topGutter || 50;
+    $("#"+container_id).width(width);
 
-    var color = "#E5E5E5";
+    // Gutter
+    var leftGutter   = opt.leftGutter   || 0;
+    var bottomGutter = opt.bottomGutter || 15;
+    var topGutter    = opt.topGutter    || 50;
+
+    var color = opt.color || "#E5E5E5";
 
     var container = Raphael(container_id, width, height);
     var X = (width - leftGutter) / data.length;
     var max = Math.max.apply(Math, data);
     var Y = (height - bottomGutter - topGutter) / max;
 
-    var path = container.path().attr({ stroke: "none"});
-    var bgp = container.path().attr({ stroke: "none", fill: color });
+    var path    = container.path().attr({ stroke: "none"});
+    var bgp     = container.path().attr({ stroke: "none", fill: color });
     var blanket = container.set();
 
     var p, bgpp;
 
     for (var i = 0, ii = data.length; i < ii; i++) {
-        var y = Math.round(height - bottomGutter - Y * data[i]),
-            x = Math.round(leftGutter + X * (i + .5));
-        if (!i) {
-            p = ["M", x, y, "C", x, y];
-            bgpp = ["M", leftGutter + X * .5, height - bottomGutter, "L", x, y, "C", x, y];
-        }
-        if (i && i < ii - 1) {
-            var Y0 = Math.round(height - bottomGutter - Y * data[i - 1]);
-            var X0 = Math.round(leftGutter + X * (i - .5));
-            var Y2 = Math.round(height - bottomGutter - Y * data[i + 1]);
-            var X2 = Math.round(leftGutter + X * (i + 1.5));
-            var a = getAnchors(X0, Y0, x, y, X2, Y2);
-            p = p.concat([a.x1, a.y1, x, y, a.x2, a.y2]);
-            bgpp = bgpp.concat([a.x1, a.y1, x, y, a.x2, a.y2]);
-        }
+      var y = Math.round(height - bottomGutter - Y * data[i]),
+      x = Math.round(leftGutter + X * (i + .5));
+      if (!i) {
+        p = ["M", x, y, "C", x, y];
+        bgpp = ["M", leftGutter + X * .5, height - bottomGutter, "L", x, y, "C", x, y];
+      }
+      if (i && i < ii - 1) {
+        var Y0 = Math.round(height - bottomGutter - Y * data[i - 1]);
+        var X0 = Math.round(leftGutter + X * (i - .5));
+        var Y2 = Math.round(height - bottomGutter - Y * data[i + 1]);
+        var X2 = Math.round(leftGutter + X * (i + 1.5));
+        var a = getAnchors(X0, Y0, x, y, X2, Y2);
+        p = p.concat([a.x1, a.y1, x, y, a.x2, a.y2]);
+        bgpp = bgpp.concat([a.x1, a.y1, x, y, a.x2, a.y2]);
+      }
 
-        blanket.push(container.rect(leftGutter + X * i, 0, X, height - bottomGutter).attr({ stroke: "none", fill: "#fff", opacity: 0 }));
-        var rect = blanket[blanket.length - 1];
+      blanket.push(container.rect(leftGutter + X * i, 0, X, height - bottomGutter).attr({ stroke: "none", fill: "#fff", opacity: 0 }));
+      var rect = blanket[blanket.length - 1];
     }
 
     p = p.concat([x, y, x, y]);
@@ -349,11 +227,6 @@ $(function(){
   $.fn.addGraph = function(data, opt) {
     drawGraph(this, data, opt);
   };
-
-  //  $(".select-filter ul").jScrollPane({ verticalDragMinHeight: 20});
-  //  $('.search_button, .candy_white_button, .candy_blue_button').mousedown(function() { $(this).addClass('active'); });
-  //  $('.search_button, .candy_white_button, .candy_blue_button').mouseup(function() { $(this).removeClass('active'); });
-  //  $('.search_button, .candy_white_button, .candy_blue_button').mouseleave(function() { $(this).removeClass('active'); });
 });
 
 
--- content/js/popovers.js
+++ content/js/popovers.js
@@ -1147,3 +1147,118 @@ var downloadPopover = (function() {
     hide: hide
   };
 })();
+
+
+/*
+* ================
+* POPOVER BINDINGS
+* ================
+*/
+
+$.fn.bindSlider = function(min, max, values) {
+  var $slider = $(this).find(".slider");
+  var $legend = $(this).find(".legend");
+
+  $slider.slider({
+    range: true,
+    min: min,
+    max: max,
+    values: values,
+    slide: function(event, ui) {
+      $legend.val("BETWEEN " + ui.values[ 0 ] + " AND " + ui.values[ 1 ] );
+    }
+  });
+
+  $legend.val( "BETWEEN " + $slider.slider( "values", 0 ) + " AND " + $slider.slider( "values", 1 ) );
+};
+
+$.fn.bindDownloadPopover = function(opt) {
+  $(this).click(function(event) {
+    downloadPopover.toggle($(this), event, opt);
+  });
+};
+
+$.fn.bindLoginPopover = function(opt) {
+  $(this).click(function(event) {
+    loginPopover.toggle($(this), event, opt);
+  });
+};
+
+$.fn.bindSortPopover = function() {
+  $(this).click(function(event) {
+    sortPopover.toggle($(this), event);
+  });
+};
+
+$.fn.bindLinkPopover = function(opt) {
+  $(this).click(function(event) {
+    linkPopover.toggle($(this), event, opt);
+  });
+};
+
+$.fn.bindHelpPopover = function(opt) {
+  $(this).click(function(event) {
+    helpPopover.toggle($(this), event, opt);
+  });
+};
+
+$.fn.bindDatePopover = function() {
+  $(this).click(function(event) {
+    datePopover.toggle($(this), event);
+  });
+};
+
+$.fn.bindSlideshow = function(opt) {
+  var $this = $(this);
+
+  var photoWidth      = 627;
+  var currentPhoto    = 0;
+  var transitionSpeed = 500;
+  var easingMethod    = "easeOutQuart";
+
+  var num_of_photos   = $this.find("div.photos > img").length - 1;
+  var downloads       = $this.find("div.download a");
+
+  var $previous_button = $this.find(".previous_slide");
+  var $next_button     = $this.find(".next_slide");
+  var $slideshow       = $this.find('.slideshow');
+
+  // The previous button is disabled by default
+  $previous_button.addClass("disabled");
+
+  // Calculate the width of the slideshow
+  $this.find(".photos").width(num_of_photos*photoWidth);
+
+  $previous_button.click(function(event) {
+    event.preventDefault();
+
+    if (currentPhoto > 0) {
+      $next_button.removeClass("disabled");
+
+      $slideshow.scrollTo('-='+photoWidth+'px', transitionSpeed, {easing:easingMethod, axis:'x'});
+      $(downloads[currentPhoto]).parent().hide();
+      currentPhoto--;
+      $(downloads[currentPhoto]).parent().show();
+
+      if (currentPhoto == 0) {
+        $previous_button.addClass("disabled");
+      }
+    }
+  });
+
+  $next_button.click(function(event) {
+    event.preventDefault();
+    if (currentPhoto < num_of_photos) {
+      $previous_button.removeClass("disabled");
+
+      $slideshow.scrollTo('+='+photoWidth+'px', transitionSpeed, {easing:easingMethod, axis:'x'});
+      $(downloads[currentPhoto]).parent().hide();
+      currentPhoto++;
+      $(downloads[currentPhoto]).parent().show();
+
+      if (currentPhoto >= num_of_photos) {
+        $next_button.addClass("disabled");
+      }
+    }
+  });
+};

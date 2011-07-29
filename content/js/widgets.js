96c8c2e09e0d89beb2c469ebbbc1ed0d114efa27
Javier Arce <javierarce@gmail.com>
2011-07-29 12:07:38 +0200

    Added more button in the autosuggest feature

--- content/js/widgets.js
+++ content/js/widgets.js
@@ -140,17 +140,6 @@ var GOD = (function() {
     });
   };
 
-  // Expose the plugin
-  $.fn.helpPopover = function(method) {
-    if (!ie6) {
-      if (methods[method]) {
-        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-      } else if (typeof method === 'object' || !method) {
-        return methods.init.apply(this, arguments);
-      }
-    }
-  };
-
   // Build popover
   function _build(data) {
     var $ps = $(_.template(data.templates.main, {name:data.name, id:data.id, title: data.title, message:data.message}));
@@ -255,6 +244,17 @@ var GOD = (function() {
     }
   }
 
+  // Expose the plugin
+  $.fn.helpPopover = function(method) {
+    if (!ie6) {
+      if (methods[method]) {
+        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+      } else if (typeof method === 'object' || !method) {
+        return methods.init.apply(this, arguments);
+      }
+    }
+  };
+
 })(jQuery, window, document);
 
 /*
@@ -343,17 +343,6 @@ var GOD = (function() {
       });
 
     });
-  };
-
-  // Expose the plugin
-  $.fn.processPopover = function(method) {
-    if (!ie6) {
-      if (methods[method]) {
-        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-      } else if (typeof method === 'object' || !method) {
-        return methods.init.apply(this, arguments);
-      }
-    }
   };
 
   // Build popover
@@ -459,6 +448,17 @@ var GOD = (function() {
       $ps.css("top", y - h - 10);
     }
   }
+
+  // Expose the plugin
+  $.fn.processPopover = function(method) {
+    if (!ie6) {
+      if (methods[method]) {
+        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+      } else if (typeof method === 'object' || !method) {
+        return methods.init.apply(this, arguments);
+      }
+    }
+  };
 
 })(jQuery, window, document);
 
@@ -580,17 +580,6 @@ var GOD = (function() {
     });
   };
 
-  // Expose the plugin
-  $.fn.selectPopover = function(method) {
-    if (!ie6) {
-      if (methods[method]) {
-        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-      } else if (typeof method === 'object' || !method) {
-        return methods.init.apply(this, arguments);
-      }
-    }
-  };
-
   // private methods
   function _build(tpl, view) {
 
@@ -698,6 +687,21 @@ var GOD = (function() {
         $ps.find(".more").show();
       }
 
+
+
+      // "Add more" action
+      $ps.find('.more').click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        var
+        $option = $(this),
+        $ps = $option.parents('.ps_container').first(),
+        data = $ps.data(store);
+
+        _toggle(e, $ps);
+      });
+
       var selected = $option.siblings('a').attr('ps-value');
 
       // Remove the element from the temporary list
@@ -708,19 +712,6 @@ var GOD = (function() {
 
       $selected_element.removeClass("hidden");
       _close($ps);
-    });
-
-    // "Add more" action
-    $('a.more').live('click', function(e) {
-      e.preventDefault();
-      e.stopPropagation();
-
-      var
-      $option = $(this),
-      $ps = $option.parents('.ps_container').first(),
-      data = $ps.data(store);
-
-      _toggle(e, $ps);
     });
 
     // Bind click action over an original element
@@ -749,6 +740,18 @@ var GOD = (function() {
         } else {
           $ps.find("a.more").hide();
         }
+        // "Add more" action
+        $ps.find('.more').click(function(e) {
+          e.preventDefault();
+          e.stopPropagation();
+
+          var
+          $option = $(this),
+          $ps = $option.parents('.ps_container').first(),
+          data = $ps.data(store);
+
+          _toggle(e, $ps);
+        });
 
         $selected = $option.parent();
         $selected.addClass('hidden');
@@ -760,6 +763,18 @@ var GOD = (function() {
       }
     });
   });
+
+  // Expose the plugin
+  $.fn.selectPopover = function(method) {
+    if (!ie6) {
+      if (methods[method]) {
+        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+      } else if (typeof method === 'object' || !method) {
+        return methods.init.apply(this, arguments);
+      }
+    }
+  };
+
 })(jQuery, window, document);
 
 
@@ -1350,16 +1365,6 @@ var linkPopover = (function() {
     });
   };
 
-  // Expose the plugin
-  $.fn.dropdownPopover = function(method) {
-    if (!ie6) {
-      if (methods[method]) {
-        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-      } else if (typeof method === 'object' || !method) {
-        return methods.init.apply(this, arguments);
-      }
-    }
-  };
   function _buildItems($ps, data) {
 
     if (data.settings.options &&  data.settings.options.links) {
@@ -1507,6 +1512,16 @@ var linkPopover = (function() {
     }
   }
 
+  // Expose the plugin
+  $.fn.dropdownPopover = function(method) {
+    if (!ie6) {
+      if (methods[method]) {
+        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+      } else if (typeof method === 'object' || !method) {
+        return methods.init.apply(this, arguments);
+      }
+    }
+  };
 })(jQuery, window, document);
 
 
@@ -2099,17 +2114,6 @@ $.fn.bindSlideshow = function(opt) {
     });
   };
 
-  // Expose the plugin
-  $.fn.selectBox = function(method) {
-    if (!ie6) {
-      if (methods[method]) {
-        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-      } else if (typeof method === 'object' || !method) {
-        return methods.init.apply(this, arguments);
-      }
-    }
-  };
-
   // Build popover
   function _build(template, data) {
     var $ps = _.template(template, {label:data.label, name:data.name, id:data.id});
@@ -2199,7 +2203,17 @@ $.fn.bindSlideshow = function(opt) {
     }
   }
 
-  $(function() {});
+  // Expose the plugin
+  $.fn.selectBox = function(method) {
+    if (!ie6) {
+      if (methods[method]) {
+        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+      } else if (typeof method === 'object' || !method) {
+        return methods.init.apply(this, arguments);
+      }
+    }
+  };
+
 
 })(jQuery, window, document);
 
